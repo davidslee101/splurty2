d425da7f25e60db83dc382d778375a10763dfcd1
david <davidslee101@gmail.com>
2015-10-08 04:05:35 +0000

    add form validation

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -6,7 +6,10 @@ class QuotesController < ApplicationController
     @quote = Quote.new
   end
   def create
-    Quote.create(quote_params)
+    @quote = Quote.create(quote_params)
+    if @quote.invalid?
+      flash[:error] = '<strong>Could not save your quote</strong>'
+    end
     redirect_to root_path
   end
 
